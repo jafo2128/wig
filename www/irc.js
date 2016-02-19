f2f7b480a32341fa0c7646ba0c394d076b210b9c
Kieran <kieran@harkin.me>
2016-02-19 11:46:31 +0000

    Add line parser

--- www/irc.js
+++ www/irc.js
@@ -7,16 +7,117 @@
 var ProtoBuf = dcodeIO.ProtoBuf
 var ByteBuffer = dcodeIO.ByteBuffer
 
+//Line parser translated from https://github.com/fluffle/goirc
+function Line(s) {
+	this.Tags = {}
+	this.Nick = null;
+	this.Ident = null;
+	this.Host = null;
+	this.Src = null;
+	this.Cmd = null;
+	this.Raw = null;
+	this.Args = [];
+	this.Time = null;
+	
+	this.Parse(s)
+}
+
+Line.prototype.Parse = function(s){
+	this.Raw = s
+	this.Time = new Date().getTime()
+	
+	if (s[0] == '@') {
+		var rawTags
+		var idx = s.indexOf(" ")
+		if (idx >= 0) {
+			rawTags = s.substring(1, idx)
+			s = s.substring(idx+1)
+		} else {
+			return null
+		}
+
+		tags = rawTags.split(";")
+		for(var x = 0; x < tags.length; x++) {
+			var tag = tags[x]
+			if (tag == "") {
+				continue
+			}
+
+			var pair = tag.replace(/[\:|\;|\s| |\r|\n]/g, "").split("=", 2)
+			if (pair.length < 2) {
+				this.Tags[tag] = ""
+			} else {
+				this.Tags[pair[0]] = pair[1]
+			}
+		}
+	}
+
+	if (s[0] == ':') {
+		var idx = s.indexOf(" ")
+		if (idx >= 0) {
+			this.Src = s.substring(1, idx)
+			s = s.substring(idx+1)
+		} else {
+			return null
+		}
+
+		this.Host = this.Src
+		var nidx = this.Src.indexOf("!")
+		var uidx = this.Src.indexOf("@")
+		if (uidx != -1 && nidx != -1) {
+			this.Nick = this.Src.substring(0, nidx)
+			this.Ident = this.Src.substring(nidx+1, uidx)
+			this.Host = this.Src.substring(uidx+1)
+		}
+	}
+
+	var args = s.split(" :", 2)
+	if (args.length > 1) {
+		var msg = args[1]
+		args = args[0].trim().split(" ")
+		args[args.length] = msg
+	} else {
+		args = args[0].trim().split(" ")
+	}
+	this.Cmd = args[0].toUpperCase()
+	if (args.length > 1) {
+		this.Args = args.slice(1)
+	}
+
+	if ((this.Cmd === "PRIVMSG"|| this.Cmd === "NOTICE") && this.Args[1].length > 2 && this.Args[1].startsWith("\x001") && this.Args[1].endsWith("\x001")) {
+		// WOO, it's a CTCP message
+		var t = this.Args[1].replace("\001" ,"").split(" ", 2)
+		if (t.length > 1) {
+			this.Args[1] = t[1]
+		}
+		var c = t[0].toUpperCase()
+		if (c === "ACTION" && this.Cmd === "PRIVMSG") {
+			this.Cmd = c
+		} else {
+			if (this.Cmd === "PRIVMSG") {
+				this.Cmd = "CTCP"
+			} else {
+				this.Cmd = "CTCPREPLY"
+			}
+			this.Args = [c].concat(this.Args)
+		}
+	}
+}
+
 function IRC() { }
 
 IRC.prototype.Proto = {}
 
-IRC.prototype.ParseLine= function(server, line){
+IRC.prototype.DecodeLine = function(s) {
+	return new Line(s)
+}
+
+IRC.prototype.ParseLine= function(server, line) {
 	if(line.indexOf("PING") === 0){
 		//auto repond with pong
 		this.SendMessage(server, line.replace("PING", "PONG"))
 	}else{
-		var ls = line.split(" ", 3)
+		var ls = this.DecodeLine(line)
 		console.log(ls)
 		this.OnPrivmsg(server,line.substring(0, line.length-1))
 	}
